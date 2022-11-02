b645ad7ac1b1b7ab69f93e6c151fef05f9ff2d72
DashaErmolich <dasha.ermolich@gmail.com>
2022-11-02 17:06:23 +0300

    feat: copy files from one folder to enother

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,44 @@
+const fs = require ('fs/promises');
+const path = require('path');
 
+const folderName = 'files';
+const folderPath = path.join(__dirname, folderName);
+
+const copyOfFolderName = 'files-copy';
+const copyOfFolderPath = path.join(__dirname, copyOfFolderName);
+
+copyDir(folderPath, copyOfFolderPath);
+
+async function copyDir(source, target) {
+    try {
+        const createDir = await fs.mkdir(target, {recursive: true});
+        const folderItems = await readFolder(source);
+        await copyFolderFiles(folderItems);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+async function readFolder(folderPath) {
+    try {
+        const folderItems = await fs.readdir(folderPath, {withFileTypes: true});
+        return folderItems;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+async function copyFolderFiles(folderItems) {
+    try {
+        for (let i = 0; i < folderItems.length; i ++) {
+            const item = folderItems[i];
+            if (!item.isDirectory()) {
+                const filePath = path.join(folderPath, item.name);
+                const copyOfFilePath = path.join(copyOfFolderPath, item.name);
+                await fs.copyFile(filePath, copyOfFilePath);
+            }
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
