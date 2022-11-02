b71e5643ed7e645072461c04543f244ab4c7e2eb
DashaErmolich <dasha.ermolich@gmail.com>
2022-11-02 16:02:29 +0300

    feat: trycatch for async functions

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -7,20 +7,23 @@ const folderPath = path.join(__dirname, folderName);
 printFilesInfo(folderPath)
 
 async function printFilesInfo(pathToFolder) {
-    const folderItems = await fs.readdir(pathToFolder, {withFileTypes: true});
+    try {
+        const folderItems = await fs.readdir(pathToFolder, {withFileTypes: true});
 
-    for (let i = 0; i < folderItems.length; i ++) {
-        const item = folderItems[i];
-        if (!item.isDirectory()) {
-            const fileExtension = getFileExtension(item.name);
-            const fileName = getFileName(item.name, fileExtension)
-            const filePath = getFilePath(pathToFolder, item.name);
-            const fileSize = await getFileSize(filePath);
-            const fileInfo = getFileOutput(fileName, fileExtension, fileSize)
-            console.log(fileInfo);
+        for (let i = 0; i < folderItems.length; i ++) {
+            const item = folderItems[i];
+            if (!item.isDirectory()) {
+                const fileExtension = getFileExtension(item.name);
+                const fileName = getFileName(item.name, fileExtension)
+                const filePath = getFilePath(pathToFolder, item.name);
+                const fileSize = await getFileSize(filePath);
+                const fileInfo = getFileOutput(fileName, fileExtension, fileSize)
+                console.log(fileInfo);
+            }
         }
+    } catch (error) {
+        console.log(error.message);
     }
-
 }
 
 function getFileExtension(filename) {
@@ -36,8 +39,12 @@ function getFilePath(pathToFolder, filename) {
 }
 
 async function getFileSize(pathToFile) {
-    const fileStats = await fs.stat(pathToFile);
-    return fileStats.size;
+    try {
+        const fileStats = await fs.stat(pathToFile);
+        return fileStats.size;
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
 function getFileOutput(name, extension, size) {
